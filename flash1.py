a2d89a1d9b8627610fa07de8d5d852a30c272a92
Luca02delflow <73719749+Luca02delflow@users.noreply.github.com>
2020-10-30 19:18:21 -0300

    Add files via upload

---
+++ flash1.py
@@ -0,0 +1,28 @@
+import time
+
+for i in range(0,253,+1):
+
+    print (i)
+
+    time.sleep(0.0001)
+
+
+
+
+
+flash_1 = 253
+
+if(flash_1==253):{print('Toplaner tiene flash, saco wea')}
+
+
+
+
+
+
+'''
+flash_1 = 253 Toplaner
+flash_2 = 253 Jungler
+flash_3 = 253 Midlaner
+flash_4 = 253 Adcarry 
+flash_5 = 253 Support
+'''
