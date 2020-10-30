a1c32225359167636d57001f2c47921fa79bc333
Flaviongas <flavyjala@hotmail.com>
2020-10-30 20:02:57 -0300

    Elegir linea

--- flash1.py
+++ flash1.py
@@ -1,25 +1,43 @@
 import time
 
-for i in range(0,285,+1):
- print (i)
- time.sleep(0.0001)
-
-
+flashtop=input(str("¿Quien gastó flash?"))
 
 flash_1 = 285
+flash_2 = 285
+flash_3 = 285
+flash_4 = 285
+flash_5 = 285
+
+if flashtop=="top":
+    for i in range(0,285,+1):
+        print (i)
+        time.sleep(0.0001)
+elif flashtop=="jg":
+    for i in range(0,285,+1):
+        print (i)
+        time.sleep(0.0001)
+elif flashtop=="mid":
+    for i in range(0,285,+1):
+        print (i)
+        time.sleep(0.0001)
+elif flashtop=="adc":
+    for i in range(0,285,+1):
+        print (i)
+        time.sleep(0.0001)
+elif flashtop=="supp":
+    for i in range(0,285,+1):
+        print (i)
+        time.sleep(0.0001)
+else: print("Pon una linea en minusculas pelotudo")
+
 
 if(flash_1==285):
     print('Toplaner tiene flash, saco wea')
-
-
-
-
-
-
-'''
-flash_1 = 285 Toplaner
-flash_2 = 285 Jungler
-flash_3 = 285 Midlaner
-flash_4 = 285 Adcarry 
-flash_5 = 285 Support
-'''
+elif(flash_2==285):
+    print('El jg tiene flash, saco wea')
+elif(flash_3==285):
+    print('Midlaner tiene flash, saco wea')
+elif(flash_4==285):
+    print('El adc tiene flash, saco wea')
+elif(flash_5==285):
+    print('El supp tiene flash, saco wea')
